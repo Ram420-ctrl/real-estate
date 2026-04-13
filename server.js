e4b8392f8e07497b71beef923c587429bf8e5da5
Ram420-ctrl <ramvijjapu143@gmail.com>
2026-04-13 17:21:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://ram:Ramesh%402005@cluster0.5yjx6yi.mongodb.net/realestate");
+// MongoDB connection
+mongoose.connect("mongodb+srv://ram:Ramesh%402005@cluster0.5yjx6yi.mongodb.net/realestate")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
+// Schema
 const propertySchema = new mongoose.Schema({
   title: String,
   price: String,
@@ -16,18 +20,22 @@ const propertySchema = new mongoose.Schema({
   description: String,
   image: String
 });
-// GET
+
+// Model
+const Property = mongoose.model("Property", propertySchema);
+
+// GET all properties
 app.get("/properties", async (req, res) => {
   const data = await Property.find();
   res.json(data);
+});
 
-const Property = mongoose.model("Property", propertySchema);
-
-// POST
+// POST new property
 app.post("/properties", async (req, res) => {
   const p = new Property(req.body);
   await p.save();
   res.json(p);
 });
 
-app.listen(5000, () => console.log("Server running"));
+// Server start
+app.listen(5000, () => console.log("Server running on port 5000"));
